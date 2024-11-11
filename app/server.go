cc10375e9318dd99e56c29154e5da85edc1f8e71
Sid Ahmed BRAHIMI <brahimi.sdhmd@gmail.com>
2024-11-11 23:48:07 +0100

    add multiple ping commands per client

--- app/server.go
+++ app/server.go
@@ -1,35 +1,57 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
 
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-  fmt.Println("Server listening on port 6379...")
+	fmt.Println("Server listening on port 6379...")
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-  defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
 
-  for {
-    conn, err := l.Accept()
-    if err != nil {
-      fmt.Println("Error accepting connection: ", err.Error())
-      os.Exit(1)
-    }
+		go handle_connection(conn)
+	}
+}
 
-    go func(c net.Conn) {
-      c.Write([]byte("+PONG\r\n"))
-      c.Close()
-    }(conn)
-  }
+func handle_connection(c net.Conn) {
+	defer c.Close()
+
+	buffer := make([]byte, 1024)
+
+	reader := bufio.NewReader(c)
+	for {
+		n, err := reader.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("Error reading stream", err, n)
+			}
+			break
+		}
+
+		receivedData := buffer[:n]
+		fmt.Printf("Received: %q\n", receivedData)
+
+		_, writeErr := c.Write([]byte("+PONG\r\n"))
+
+		if writeErr != nil {
+			fmt.Println("Error write stream to client", err)
+			break
+		}
+	}
 }
